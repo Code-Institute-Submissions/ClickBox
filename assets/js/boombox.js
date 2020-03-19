e1d4ef973177dfa3a04404ba48f43b82d868cfe0
mauroindigne <mauroindigne@gmail.com>
2020-03-19 19:28:41 +0000

    added welcome screen animation, compleate

--- assets/js/boombox.js
+++ assets/js/boombox.js
@@ -40,26 +40,32 @@ $(document).ready(function() {
             $("#screen-button").attr('class', 'screen-button');
             $("#screen-switch").attr('class', 'light-switch-screen-bg');
             $(document.getElementById("volume-box").style.borderColor= "#8a8a8aff");
+            $(document.getElementById("volume-bar").style.backgroundColor= "#8a8a8aff");
+            $("#screen-top-up").attr('class', 'screen-top-up');
+            $("#screen-bottom-up").attr('class', 'screen-bottom-up');
         }
         else {
 
             $("#screen-button").attr('class', 'screen-button-on');
             $("#screen-switch").attr('class', 'light-switch-screen-bg-on');
             $(document.getElementById("volume-box").style.borderColor= "orange");
-        }
+            $(document.getElementById("volume-bar").style.backgroundColor= "orange");
+            $("#screen-top-up").attr('class', 'screen-top-down');
+            $("#screen-bottom-up").attr('class', 'screen-bottom-down');
+        }   
         
     });
     
 });
 
 
-/*----------------------------IOS Switch Screen ----------------- */
+/*----------------------------color Switch Screen ----------------- */
 function changeRed() {
     document.getElementById("background").style.backgroundColor = "red";
     document.getElementById("volume-box").style.borderColor= "red";
     document.getElementById("speaker-small-color-changer").style.backgroundColor= "red";
     document.getElementById("speaker-big-color-changer").style.backgroundColor= "red";
-   
+    document.getElementById("volume-bar").style.backgroundColor= "red";
 
 };
 function changeBlue() {
@@ -67,12 +73,14 @@ function changeBlue() {
     document.getElementById("volume-box").style.borderColor= "blue";
     document.getElementById("speaker-small-color-changer").style.backgroundColor= "blue";
     document.getElementById("speaker-big-color-changer").style.backgroundColor= "blue";
+    document.getElementById("volume-bar").style.backgroundColor= "blue";
 }
 function changeGreen() {
     document.getElementById("background").style.backgroundColor = "green"
     document.getElementById("volume-box").style.borderColor= "green";
     document.getElementById("speaker-small-color-changer").style.backgroundColor= "green";
     document.getElementById("speaker-big-color-changer").style.backgroundColor= "green";
+    document.getElementById("volume-bar").style.backgroundColor= "green";
 }
 /*----------------------------Screen Zoom Effect ----------------- */
 $(document).on('click', '#speaker-top', function() {
@@ -81,6 +89,7 @@ $(document).on('click', '#speaker-top', function() {
     $(this).find('#volume-box').addClass('volume-box').removeClass('volume-box-hide');
     $(this).children().addClass('inner-screen-area').removeClass('inner-screen-area-hide');
     $(this).siblings().addClass('speaker-box-hide').removeClass('speaker-box-show');
+    $(this).find('#welcome').removeClass('welcome-hide').addClass('welcome-show');
     
     
 });
@@ -90,4 +99,6 @@ $(document).on('click', '#background', function() {
     $('#speaker-top').removeClass('screen-top-zoom').addClass('screen-top');
     $('#speaker-box').removeClass('speaker-box-hide').addClass('speaker-box-show');
     $('#volume-box').addClass('volume-box-hide').removeClass('volume-box');
+    $('#welcome').removeClass('welcome-show').addClass('welcome-hide');
 });
+
